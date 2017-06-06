5ba5da26cea6119b3104f23df2d9dc79eae19032
IStrateg <ewseew.alex@mail.ru>
2017-06-06 13:08:49 +0400

    Created base mod structure.

---
+++ src/main/java/ru/strateg/newworld/NewWorld.java
@@ -0,0 +1,20 @@
+package ru.strateg.newworld;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Created by Alex on 06.06.2017.
+ */
+@Mod(modid = NewWorld.MODID, version = NewWorld.VERSION)
+public class NewWorld
+{
+    public static final String MODID = "newworld";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
